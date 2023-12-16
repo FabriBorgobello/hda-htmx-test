bb8e43f5c5ac95ff0bde543f5cb28054eb7d02a9
Fabricio Borgobello <fborgobello@gmail.com>
2023-12-16 15:00:47 +0000

    TailwindCSS CDN

--- src/jsx/pages/Layout.tsx
+++ src/jsx/pages/Layout.tsx
@@ -3,8 +3,14 @@ import { NavBar } from '@/jsx/partials/NavBar';
 
 export const Layout: FC = ({ children }) => {
   return (
-    <html>
+    <html lang="en">
       <body>
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>My App</title>
+          <script src="https://cdn.tailwindcss.com"></script>
+        </head>
         <NavBar />
         <div>{children}</div>
       </body>
